733db928886fdbba9f4de7edaa8da693f655d027
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-04-11 11:44:23 +0545

    firebase config setup, getting data, saving data to firebase, delete data

---
+++ app.js
@@ -0,0 +1,51 @@
+const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-cafe-form");
+
+//create element and render cafe
+function renderCafe(doc) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+  let cross = document.createElement("div");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+  cross.textContent = "x";
+
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross);
+  cafeList.appendChild(li);
+
+  //deleting data
+  cross.addEventListener("click", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("cloudcafe").doc(id).delete();
+  });
+}
+
+//getting data
+db.collection("cloudcafe")
+  .where("city", "==", "sunwal")
+  .orderBy("name")
+  .get()
+  .then((snapshot) => {
+    // console.log(snapshot.docs);
+    snapshot.docs.forEach((doc) => {
+      renderCafe(doc);
+    });
+  });
+
+//saving data
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  db.collection("cloudcafe").add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  form.name.value = "";
+  form.city.value = "";
+});
